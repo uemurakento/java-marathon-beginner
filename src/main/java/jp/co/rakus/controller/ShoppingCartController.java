54c02b600dcb07ec8ae7ea546199b2d541d56fe5
uemurakento <kento.uemura@rakus.co.jp>
2018-10-31 11:17:12 +0900

    25kmの完成

--- src/main/java/jp/co/rakus/controller/ShoppingCartController.java
+++ src/main/java/jp/co/rakus/controller/ShoppingCartController.java
@@ -13,6 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.rakus.domain.Item;
 
+/**
+ * ショッピングカートの商品の追加、削除の結果を出力するコントローラー
+ * 
+ * @author kento.uemura
+ *
+ */
 @Controller
 @RequestMapping("/shoppingCart")
 public class ShoppingCartController {
@@ -20,24 +26,65 @@ public class ShoppingCartController {
 	private ServletContext application;
 	@Autowired
 	private HttpSession session;
-	
+
+	/**
+	 * 初期画面、更新された画面を表示する.
+	 * 
+	 * @param model
+	 * @return 表示する画面
+	 */
+	@SuppressWarnings("unchecked")
 	@RequestMapping("/")
 	public String index(Model model) {
-		List<Item> items = new LinkedList<>();
-		items.add(new Item("手帳ノート",1000));
-		items.add(new Item("文房具セット",1500));
-		items.add(new Item("ファイル",2000));
-		application.setAttribute("items", items);
-		session.setAttribute("cartItems", new LinkedList<Item>());
+		if (application.getAttribute("items") == null) {
+			List<Item> items = new LinkedList<>();
+			items.add(new Item("手帳ノート", 1000));
+			items.add(new Item("文房具セット", 1500));
+			items.add(new Item("ファイル", 2000));
+			application.setAttribute("items", items);
+			session.setAttribute("cartItems", new LinkedList<Item>());
+			Integer sum = 0;
+			model.addAttribute("sum", sum);
+		}
 		Integer sum = 0;
-		model.addAttribute("sum",sum);
+		List<Item> cartItems = (List<Item>) session.getAttribute("cartItems");
+		for (Item item : cartItems) {
+			sum += item.getPrice();
+		}
+		model.addAttribute("sum", sum);
 		return "itemandcart";
 	}
-	
+
+	/**
+	 * カートに商品を追加する.
+	 * 
+	 * @param itemNum
+	 *            追加する商品のリストの番号
+	 * @return indexにリダイレクト
+	 */
+	@SuppressWarnings("unchecked")
 	@RequestMapping("/inCart")
-	public String inCart(String inItemName) {
-		//ここで受け取ったアイテム名からアプリケーションスコープの中のリストの中のItemをとってきて、セッションスコープのほうのリストに格納する
-		return null;
+	public String inCart(int itemNum) {
+		List<Item> items = (List<Item>) application.getAttribute("items");
+		Item item = items.get(itemNum);
+		List<Item> list = (LinkedList<Item>) session.getAttribute("cartItems");
+		list.add(item);
+		return "redirect:/shoppingCart/";
+	}
+
+	/**
+	 * カートの商品を削除する.
+	 * 
+	 * @param itemNum
+	 *            削除する商品のリストの番号
+	 * @return indexにリダイレクト
+	 */
+	@SuppressWarnings("unchecked")
+	@RequestMapping("/outCart")
+	public String outCart(int itemNum) {
+		List<Item> items = (List<Item>) session.getAttribute("cartItems");
+		items.remove(itemNum);
+		return "redirect:/shoppingCart/";
 	}
 
 }
--- src/main/webapp/WEB-INF/views/itemandcart.jsp
+++ src/main/webapp/WEB-INF/views/itemandcart.jsp
@@ -10,16 +10,29 @@
 <body>
 <h2>商品一覧</h2>
 <table>
-<c:forEach var="item" items="${items}">
+<c:forEach var="item" items="${items}" varStatus="status">
 <tr>
 <td><c:out value="${item.name}"/> <fmt:formatNumber value="${item.price}" pattern="###,###円"/></td>
 <td><form action="${pageContext.request.contextPath}/shoppingCart/inCart" method="post">
-<input type="hidden" value="${item.name}" name="itemName">
+<input type="hidden" value="${status.index}" name="itemNum">
 <input type="submit" value="カートへ">
 </form></td>
 </tr>
 </c:forEach>
 </table>
-
+<h2>ショッピングカート</h2>
+<table>
+<c:forEach var="item" items="${cartItems}" varStatus="status">
+<tr>
+<td><c:out value="${item.name}"/> <fmt:formatNumber value="${item.price}" pattern="###,###円"/></td>
+<td><form action="${pageContext.request.contextPath}/shoppingCart/outCart" method="post">
+<input type="hidden" value="${status.index}" name="itemNum">
+<input type="submit" value="削除">
+</form></td>
+</tr>
+</c:forEach>
+</table>
+<h2>合計</h2>
+<fmt:formatNumber value="${sum}" pattern="###,###円"/>
 </body>
 </html>
